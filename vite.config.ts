c10fd78e2e59cbe046ffe3ae68dcfa6bb6fa90eb
zeth <zethwporter@gmail.com>
2025-12-17 22:38:06 -0700

    another go

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,26 @@ const config = defineConfig({
     viteReact(),
   ],
   ssr: {
-    noExternal: ['@tabler/icons-react'],
+    noExternal: [
+      '@tabler/icons-react',
+      '@tanstack/react-router',
+      '@tanstack/react-router-ssr-query',
+      '@tanstack/react-start',
+    ],
+    optimizeDeps: {
+      include: [
+        '@tanstack/react-router',
+        '@tanstack/react-router-ssr-query',
+        '@tanstack/react-start',
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 })
 
